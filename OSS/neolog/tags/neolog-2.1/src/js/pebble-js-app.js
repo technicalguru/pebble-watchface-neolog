4cc8b89795dc52a4fcb58b70e1246aa354bb5fa3
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2015-09-30 10:21:11 +0000

    Release 2.1

---
+++ OSS/neolog/tags/neolog-2.1/src/js/pebble-js-app.js
@@ -0,0 +1,50 @@
+
+Pebble.addEventListener('ready', function(e) {
+	console.log('JavaScript app ready and running!');
+});
+
+Pebble.addEventListener('showConfiguration', function(e) {
+	var watch = null;
+	if (typeof Pebble.getActiveWatchInfo == 'function') {
+		watch = Pebble.getActiveWatchInfo();
+	} else {
+		watch = {
+			platform : 'aplite',
+			language : 'en_US'
+		};
+	}
+
+	// Show config page
+	var file = 'index.html';
+	var lang = '';
+	if (watch != null) {
+		if (watch.platform == 'aplite') {
+			file = 'index_aplite.html';
+		}
+		if (navigator.language == 'de_DE') {
+			lang = 'de/';
+		}
+	}
+
+	var url = 'http://pebble.ralph-schuster.eu/neolog/2.1/config/'+lang+file;
+	console.log('Opening config URL: '+url);
+	Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+	var configData = JSON.parse(decodeURIComponent(e.response));
+
+	console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+	if (configData.backgroundColor) {
+		Pebble.sendAppMessage({
+			backgroundColor: parseInt(configData.backgroundColor, 16),
+			foregroundColor: parseInt(configData.foregroundColor, 16),
+			displayStatusBar: configData.displayStatusBar
+		}, function() {
+			console.log('Send successful!');
+		}, function() {
+			console.log('Send failed!');
+		});
+	}
+});
